e2c48e622031a1a3f6f4e75d7e892a5ba072f38d
Nguyen Thanh Chuong <chuongntgch16113@fpt.edu.vn>
2020-05-13 14:16:58 +0700

    part 7

--- app/forms.py
+++ app/forms.py
@@ -30,3 +30,8 @@ class EditProfileForm(FlaskForm):
 	username = StringField('Username', validators=[DataRequired()])
 	about_me = TextAreaField('About me', validators = [Length(min=0,max=140)])
 	submit = SubmitField('Submit')
+
+	def __init__(self,original_username,*args,**kwargs):
+		super(EditProfileForm,self).__init__(*args,**kwargs)
+		if user is not None:
+			raise ValidationError('Please use a different user name.')
--- app/routes.py
+++ app/routes.py
@@ -87,7 +87,7 @@ def before_request():
 @app.route('/edit_profile',methods=['GET','POST'])
 @login_required
 def edit_profile():
-	form = EditProfileForm()
+	form = EditProfileForm(current_user.username)
 	if form.validate_on_submit():
 		current_user.username = form.username.data
 		current_user.about_me = form.about_me.data
@@ -97,4 +97,4 @@ def edit_profile():
 	elif request.method =='GET':
 		form.username.data = current_user.username
 		form.about_me.data = current_user.about_me
-	return render_template('user.html',user=user,posts =posts)
+	return render_template('edit_profile.html',title='Edit profile',form=form)
