553c5d30e70988be0c99cd10824ac39be9e36a79
Nguyen Thanh Chuong <chuongntgch16113@fpt.edu.vn>
2020-05-14 11:47:22 +0700

    part 9

--- app/routes.py
+++ app/routes.py
@@ -8,29 +8,39 @@ from app import db
 from app.forms import RegistrationForm
 from app.forms import LoginForm
 from app.forms import EditProfileForm
+from app.forms import PostForm
 from flask_login import current_user,login_user
 from flask_login import login_required
 from flask_login import logout_user
 from app.models import User
+from app.models import Post
 from datetime import datetime
 
-@app.route('/')
-@app.route('/index')
+@app.route('/',methods=['GET','POST'])
+@app.route('/index',methods=['GET','POST'])
 @login_required
 def index():
-	user = {'username': 'Miguel'}
-	posts = [
-		{
-		'author' : {'username': 'John'},
-		'body' : 'Beautiful day in  Portland!'
-		},
+	form = PostForm()
+	if form.validate_on_submit():
+		post = Post(body = form.post.data,author = current_user)
+		db.session.add(post)
+		db.session.commit()
+		flash('Your post is now live!')
+		return redirect(url_for('index'))
+	posts = current_user.followed_posts().all()
+	return render_template("index.html",title='Home Page',form=form,posts=posts)
+	page = requests.args.get('page',1,type=int)
+	posts = current_user.followed_posts().paginate(page,app.config['POST_PER_PAGE'],False)
+	return render_templat('index.html',title='Home',form=form,posts=posts.items)
+	page = request.args.get('page',1,type=int)
+	posts = current_user.followed_posts().paginate(page,app.config['POST_PER_PAGE'],False)
+	next_url = url_for('index',page=posts.next_num,)\
+		if posts.has_next else None
+	prev_url = url_for('index',page = posts.prev_num)\
+		if posts.has_prev else None
+	return render_template('index.html',title="Home",form=form,posts=posts.items,next_url=next_url,prev_url=prev_url)
 
-		{
-		'author' : {'username': 'Susan'},
-		'body' :'The Avengers movies was so cool!'
-		}
-]
-	return render_template("index.html",title='Home Page',posts=posts)
+
 
 @app.route('/login',methods=['GET','POST'])
 def login():
@@ -49,10 +59,12 @@ def login():
 		return redirect(next_page)
 	return render_template('login.html',title='Sign In',form=form)
 
+
 @app.route('/logout')
 def logout():
 	logout_user()
 	return redirect(url_for('index'))
+
 
 @app.route('/register',methods=['GET','POST'])
 def register():
@@ -68,21 +80,26 @@ def register():
 		return redirect(url_for('login'))
 	return render_template('register.html',title = 'Register', form = form)
 
+
 @app.route('/user/<username>')
 @login_required
 def user(username):
 	user = User.query.filter_by(username = username ).first_or_404()
-	posts = [
-		{'author': user,'body' : 'Test post #1' },
-		{'author': user,'body' : 'Test post #2'}
-	]
-	return render_template('user.html',user=user,posts =posts)
+	page = request.args.get('page',1,type=int)
+	posts = user.posts.order_by(Post.timestamp.desc()).paginate(page,app.config['POST_PER_PAGE'],False)
+	next_url = url_for('user',username= user.username,page=posts.next_num)\
+		if posts.has_next else None
+	prev_url = url_for('user',username= user.username,page=posts.prev_num)\
+		if posts.has_prev else None
+	return render_template('user.html',user=user,posts =posts.items,next_url=next_url,prev_url=prev_url)
+
 
 @app.before_request
 def before_request():
 	if current_user.is_authenticated:
 		current_user.last_seen = datetime.utcnow()
 		db.session.commit()
+
 
 @app.route('/edit_profile',methods=['GET','POST'])
 @login_required
@@ -98,6 +115,8 @@ def edit_profile():
 		form.username.data = current_user.username
 		form.about_me.data = current_user.about_me
 	return render_template('edit_profile.html',title='Edit profile',form=form)
+
+
 @app.route('/follow/<username>')
 @login_required
 def follow(username):
@@ -109,8 +128,10 @@ def follow(username):
 		flash('You cannot follow yourself!')
 		return redirect(url_for('user',username=username))
 	current_user.follow(user)
+	db.session.commit()
 	flash('you are following{}!'.format(username))
 	return redirect(url_for('user',username=username))
+
 
 @app.route('/unfollow/<username>')
 @login_required
@@ -126,3 +147,15 @@ def unfollow(username):
 	db.session.commit()
 	flash('You are not following {}.'.format(username))
 	return redirect(url_for('user',username=username))
+
+
+@app.route('/explore')
+@login_required
+def explore():
+	page = request.args.get('page',1,type=int)
+	posts = Post.query.order_by(Post.timestamp.desc()).paginate(page,app.config['POST_PER_PAGE'],False)
+	next_url = url_for('explore',page=posts.next_num)\
+		if posts.has_next else None
+	prev_url = url_for('explore',page=posts.prev_url)\
+		if posts.has_prev else None
+	return render_template('index.html',title='Explore',posts=posts.items,next_url=next_url,prev_url=prev_url)
--- config.py
+++ config.py
@@ -12,3 +12,4 @@ class Config(object):
 	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
 	MAIL_PASSWORD = os.environ.get('MAIL PASSWORD')
 	ADMINS = ['you-email@example.com']
+	POST_PER_PAGE = 25
